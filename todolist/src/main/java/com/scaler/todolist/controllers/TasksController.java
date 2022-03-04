a66f090d4c85671d0ff32f22f93ced93cf71b027
Chandrakanth Nethi <chandrakanth.nethi@gmail.com>
2022-03-04 20:49:23 +0530

    Complete assignment

--- todolist/src/main/java/com/scaler/todolist/controllers/TasksController.java
+++ todolist/src/main/java/com/scaler/todolist/controllers/TasksController.java
@@ -1,28 +1,29 @@
 package com.scaler.todolist.controllers;
 
 import com.scaler.todolist.models.Task;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.TreeMap;
 
 @RequestMapping("/tasks")
 @RestController
 public class TasksController {
 
-    private ArrayList<Task> taskList = new ArrayList<>();
+    private Integer idCounter = 0;
+    private TreeMap<Integer, Task> taskMap;
 
     @GetMapping("/")
-    ResponseEntity<List<Task>> getAllTasks() {
-        return ResponseEntity.ok(taskList);
+    ResponseEntity<Collection<Task>> getAllTasks() {
+        return ResponseEntity.ok(taskMap.values());
     }
 
     @PostMapping("/")
     ResponseEntity<Task> addNewTask(@RequestBody Task task) {
-        Task taskToAdd = new Task(task.getName());
-        taskList.add(taskToAdd);
+        Task taskToAdd = new Task(idCounter++, task.getName());
+        taskMap.put(taskToAdd.getId(), taskToAdd);
         return ResponseEntity.status(201).body(taskToAdd);
     }
 
@@ -38,6 +39,29 @@ public class TasksController {
      *          delete task no 5 (response with correct HTTP code)
      *          if task 5 does not exist, send 404
      */
+    @GetMapping("/:id")
+    ResponseEntity<Task> getTask(@RequestBody Task task) {
+        if(taskMap.containsKey(task.getId())) {
+            return ResponseEntity.status(200).body(taskMap.get(task.getId()));
+        }
+        return ResponseEntity.status(404).body(new Task());
+    }
 
+    @PatchMapping("/:id")
+    ResponseEntity<String> updateTask(@RequestBody Task task) {
+        if(taskMap.containsKey(task.getId())) {
+            taskMap.get(task.getId()).setName(task.getName());
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Task successfully updated");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+    }
 
+    @DeleteMapping("/:id")
+    ResponseEntity<String> deleteTask(@RequestBody Task task) {
+        if(taskMap.containsKey(task.getId())) {
+            taskMap.remove(task.getId());
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Successfully deleted");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+    }
 }
