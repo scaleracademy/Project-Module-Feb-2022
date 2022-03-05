b4aefda9f6c2b6f233d4d5eadb87956133dfd380
alfred-thats-angry <55573776+batman-that-laughs@users.noreply.github.com>
2022-03-05 22:52:39 +0530

    Modified code to be better readable. Added generic not found responses.

--- taskmanager/src/main/java/com/scaler/taskmanager/notes/NotesController.java
+++ taskmanager/src/main/java/com/scaler/taskmanager/notes/NotesController.java
@@ -1,30 +1,61 @@
 package com.scaler.taskmanager.notes;
 
-import com.scaler.taskmanager.tasks.UpdateTaskRequestBody;
-import org.springframework.http.HttpStatus;
+import com.scaler.taskmanager.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/notes")
+@RequestMapping(path = "/tasks/{id}/notes")
 public class NotesController {
 
-    private final NotesService notesService;
+    @Autowired
+    NotesService notesService;
 
-    public NotesController(NotesService notesService) {
-        this.notesService = notesService;
+    @PostMapping(path = "")
+    ResponseEntity<NotesResponseBody> createNotes(@PathVariable Long id, @RequestBody CreateNoteRequestBody requestBody){
+        NotesResponseBody responseBody = notesService.addNote(requestBody, id);
+        return null != responseBody ? ResponseEntity.created(
+                URI.create(Constants.BASE_URL + "/notes/" + responseBody.getId())
+        ).body(responseBody) : ResponseEntity.notFound().build();
     }
 
-    @GetMapping(path = "/{id}")
-    ResponseEntity<NotesResponseBody> getNoteById(@PathVariable Long id){
-        return ResponseEntity.ok(notesService.getNoteById(id));
+    @GetMapping(path = "")
+    ResponseEntity<List<NotesResponseBody>> getNotesByTaskId(@PathVariable Long id){
+        List<NotesResponseBody> response = notesService.getNotesByTask(id);
+        return !response.isEmpty() ? ResponseEntity.ok(response) : ResponseEntity.notFound().build();
     }
 
-    @PutMapping(path = "")
-    ResponseEntity<NotesResponseBody> updateNote(@RequestBody UpdateNoteRequestBody requestBody){
-        NotesResponseBody response = notesService.addNote(requestBody);
-        return ResponseEntity.accepted().body(response);
+    @PutMapping(path = "/{noteid}")
+    ResponseEntity<NotesResponseBody> updateNote(@PathVariable Long id, @PathVariable Long noteid, @RequestBody CreateNoteRequestBody requestBody){
+        NotesResponseBody response = notesService.updateNote(requestBody, noteid, id);
+        return null != response ?ResponseEntity.accepted().body(response) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping(path = "")
+    ResponseEntity<Long> deleteNotesByTask(@PathVariable Long id){
+        boolean removed = notesService.deleteByTask(id);
+        if(!removed)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.accepted().body(id);
+    }
+
+
+    @DeleteMapping(path = "/{noteid}")
+    ResponseEntity<Map<Long, Long>> deleteParticularNoteForTask(@PathVariable Long id, @PathVariable Long noteid){
+        boolean removed = notesService.delete(noteid, id);
+        if(!removed)
+            return ResponseEntity.notFound().build();
+        else{
+            Map<Long, Long> response = new HashMap<>();
+            response.put(id, noteid);
+            return ResponseEntity.accepted().body(response);
+        }
     }
 }
